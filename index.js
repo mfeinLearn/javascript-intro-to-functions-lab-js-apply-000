d302910c7473af3eb3ff1a716d916f48fde87af9
Malcome Fein <mfein90@gmail.com>
2018-06-19 17:44:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@
     }
     
     function sayHiToGrandma(string) {
-      var lowercase = "hello!"
-      lowercase.toLowerCase() === lowercase
-      return "I can\'t hear you!"
+      return string.toLowerCase() === "I can\'t hear you!"
+      return string.toUpperCase() === "I can\'t hear you!"
+      return string.toLowerCase() === "I can\'t hear you!"
     }
