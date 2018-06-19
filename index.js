30571bb05235c11323a2a73cb03566bb1401407a
Malcome Fein <mfein90@gmail.com>
2018-06-19 17:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,31 @@
       lowercase.toLowerCase() === lowercase
       return "I can\'t hear you!"
     }
+
+    // function shout(string) {
+    //   return string.toUpperCase()
+    // }
+    
+    // function whisper(string) {
+    //   return string.toLowerCase()
+    // }
+    
+    // function logShout(string) {
+    //   console.log(string.toUpperCase())
+    // }
+    
+    // function logWhisper(string) {
+    //   console.log(string.toLowerCase())
+    // }
+    
+    // function sayHiToGrandma(string) {
+    //   var uppercase = "HELLO!"
+    //   uppercase.toUpperCase() === uppercase
+    //   return "YES INDEED!"
+    // }
+    
+    // function sayHiToGrandma(string) {
+    //   var lowercase = "hello!"
+    //   lowercase.toLowerCase() === lowercase
+    //   return "I can\'t hear you!"
+    // }
