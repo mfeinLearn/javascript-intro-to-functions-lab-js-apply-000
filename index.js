88240853a861540f3b05a5feb84d09fdaf4820af
Malcome Fein <mfein90@gmail.com>
2018-06-19 17:20:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,10 @@
     
     function sayHiToGrandma(string) {
       var uppercase = "HELLO!"
-      var lowercase = "hello!"
-      
-      if (uppercase.toUpperCase() === uppercase) {
-      return "YES INDEED!";
-      } else if (lowercase.toLowerCase() === lowercase ) {
-        return "I can\'t hear you!";
-      }
+      uppercase.toUpperCase() === uppercase
+ 
+      var lowercase = 'hello!'
+      lowercase.toLowerCase() === lowercase
     }
 
     // function shout(string) {
