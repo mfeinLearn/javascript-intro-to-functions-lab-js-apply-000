6cb80ae388be84de8c32ee0123c80273fb0eb726
Malcome Fein <mfein90@gmail.com>
2018-06-19 17:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,37 @@
     
     function sayHiToGrandma(string) {
       var uppercase = "HELLO!"
-      uppercase.toUpperCase() === uppercase
-      return "YES INDEED!"
+      if (string === uppercase ) {
+        return "YES INDEED!"
+      }
     }
     
-    function sayHiToGrandma(string) {
-      var lowercase = "hello!"
-      lowercase.toLowerCase() === lowercase
-      return "I can\'t hear you!"
-    }
+
+// ///
+//     function shout(string) {
+//       return string.toUpperCase()
+//     }
+    
+//     function whisper(string) {
+//       return string.toLowerCase()
+//     }
+    
+//     function logShout(string) {
+//       console.log(string.toUpperCase())
+//     }
+    
+//     function logWhisper(string) {
+//       console.log(string.toLowerCase())
+//     }
+    
+//     function sayHiToGrandma(string) {
+//       var uppercase = "HELLO!"
+//       uppercase.toUpperCase() === uppercase
+//       return "YES INDEED!"
+//     }
+    
+//     function sayHiToGrandma(string) {
+//       var lowercase = "hello!"
+//       lowercase.toLowerCase() === lowercase
+//       return "I can\'t hear you!"
+//     }
